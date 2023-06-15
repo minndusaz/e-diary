d1f9bd14786cd2afb7d086d50cb52effada53978
RenaldasZ <renald.zvega@gmail.com>
2023-06-15 13:30:25 +0300

    overthinking

--- e_diary/diary_app/serializers.py
+++ e_diary/diary_app/serializers.py
@@ -1,15 +1,24 @@
 from rest_framework import serializers
-from .models import Student, Teacher, Subject
+from .models import Student, Teacher, Subject, Parent
+
 
 class StudentSerializer(serializers.ModelSerializer):
     class Meta:
         model = Student
         fields = '__all__'  # Include all fields from the Student model
 
+
 class TeacherSerializer(serializers.ModelSerializer):
     class Meta:
         model = Teacher
         fields = '__all__'  # Include all fields from the Teacher model
+
+
+class ParentSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Parent
+        fields = '__all__'  # Include all fields from the Teacher model
+
 
 class SubjectSerializer(serializers.ModelSerializer):
     class Meta:

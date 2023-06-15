7ac7ee5b2739c030dfb8adfb64a0c253d47e9892
RenaldasZ <renald.zvega@gmail.com>
2023-06-15 11:46:48 +0300

    altered tables added subject as a new class, added serializers and added the basic template

--- e_diary/diary_app/urls.py
+++ e_diary/diary_app/urls.py
@@ -1,6 +1,13 @@
-# from django.urls import path
-# from . import views
+from . import views
+from django.urls import path
+from .views import StudentListAPIView, StudentDetailAPIView, TeacherListAPIView, TeacherDetailAPIView, SubjectListAPIView, SubjectDetailAPIView
 
-# urlpatterns = [
-#     path('', views.index, name='index')
-# ]
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('students/', StudentListAPIView.as_view(), name='student-list'),
+    path('students/<int:pk>/', StudentDetailAPIView.as_view(), name='student-detail'),
+    path('teachers/', TeacherListAPIView.as_view(), name='teacher-list'),
+    path('teachers/<int:pk>/', TeacherDetailAPIView.as_view(), name='teacher-detail'),
+    path('subjects/', SubjectListAPIView.as_view(), name='subject-list'),
+    path('subjects/<int:pk>/', SubjectDetailAPIView.as_view(), name='subject-detail'),
+]
--- e_diary/diary_app/views.py
+++ e_diary/diary_app/views.py
@@ -1,3 +1,34 @@
+from rest_framework.generics import ListAPIView, RetrieveAPIView
+from .models import Student, Teacher, Subject
+from .serializers import StudentSerializer, TeacherSerializer, SubjectSerializer
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, 'diary_app/index.html')
+
+
+
+
+class StudentListAPIView(ListAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class StudentDetailAPIView(RetrieveAPIView):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+class TeacherListAPIView(ListAPIView):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
+class TeacherDetailAPIView(RetrieveAPIView):
+    queryset = Teacher.objects.all()
+    serializer_class = TeacherSerializer
+
+class SubjectListAPIView(ListAPIView):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
+
+class SubjectDetailAPIView(RetrieveAPIView):
+    queryset = Subject.objects.all()
+    serializer_class = SubjectSerializer
